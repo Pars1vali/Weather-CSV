5a4661d1f8b06b7eb386d20356f8a638a32a3761
Chumak Vladimir <parsival427@gmail.com>
2023-02-20 15:43:09 +0300

    add csv download

--- downloadCSV.js
+++ downloadCSV.js
@@ -14,7 +14,20 @@ export function downloadJSON(data) {
     const csv = parser.parse(data)
     console.log(csv)
     // тут скачивание csv
+    downloadBlob(csv, `${data.name}.csv`, 'text/csv;charset=utf-8;')
   } catch (err) {
     console.error(err)
   }
+}
+
+function downloadBlob(content, filename, contentType) {
+  // Create a blob
+  var blob = new Blob([content], { type: contentType })
+  var url = URL.createObjectURL(blob)
+
+  // Create a link to download it
+  var pom = document.createElement('a')
+  pom.href = url
+  pom.setAttribute('download', filename)
+  pom.click()
 }
